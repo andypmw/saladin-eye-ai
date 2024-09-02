73254ce1f859b4ae982ff09a50f99baab38c9039
Andy Primawan <andypmw@users.noreply.github.com>
2024-09-02 11:56:51 +0700

    feat(media-service): implement ListDate ListHourByDate

--- saladin-eye-ai-microservices/media-service/internal/objectstorage/types.go
+++ saladin-eye-ai-microservices/media-service/internal/objectstorage/types.go
@@ -7,4 +7,6 @@ type ObjectStorageIface interface {
 	GeneratePresignedUploadUrl(ctx context.Context, path string, durationMinute int) (string, error)
 	GeneratePresignedDownloadUrl(ctx context.Context, path string, durationMinute int) (string, error)
 	ListObjectsByPrefix(ctx context.Context, prefix string) ([]string, error)
+	ListDate(ctx context.Context, deviceId string) ([]string, error)
+	ListHourByDate(ctx context.Context, deviceId, date string) ([]string, error)
 }
